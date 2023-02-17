c7d37dcd5ee5598b17ddc6853d8f6231be8a013c
Shamim742 <shamimislam2742@gmail.com>
2023-02-17 08:41:07 +0600

    cart-functionality

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ document.getElementById('first-card').addEventListener('click', function () {
     const productName = document.getElementById('first-name').innerText;
     const productPrice = document.getElementById('first-price').innerText;
     const productQuantity = document.getElementById('first-quantity').innerText;
-
     const container = document.getElementById('table-container');
-    
+
+    const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
+
     const tr = document.createElement('tr');
     tr.innerHTML = `
     <td>${1}</td>
     <td>${productName}</td>
     <td>${productPrice}</td>
     <td>${productQuantity}</td>
+    <td>${priceTotal}</td>
     `;
     container.appendChild(tr);
 
