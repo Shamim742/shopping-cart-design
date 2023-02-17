de1a3d1b159374ba230008d7a8e73b0db1392179
Shamim742 <shamimislam2742@gmail.com>
2023-02-17 08:47:27 +0600

    add every functionality to js function

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ document.getElementById('first-card').addEventListener('click', function () {
     const productName = document.getElementById('first-name').innerText;
     const productPrice = document.getElementById('first-price').innerText;
     const productQuantity = document.getElementById('first-quantity').innerText;
-    const container = document.getElementById('table-container');
 
     const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
+    displayData(productName, productPrice, productQuantity, priceTotal)
+});
 
+function displayData(productName, productPrice, productQuantity, priceTotal) {
+    const container = document.getElementById('table-container');
     const tr = document.createElement('tr');
     tr.innerHTML = `
     <td>${1}</td>
@@ -17,5 +20,4 @@ document.getElementById('first-card').addEventListener('click', function () {
     <td>${priceTotal}</td>
     `;
     container.appendChild(tr);
-
-});
+};
